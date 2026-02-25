f416046bad67983290d8e4c59a555e303c7c249c
Kute <voductu2007@gmail.com>
2026-02-25 20:44:34 +0700

    wa

--- src/xrSound/SoundRender_CoreA.cpp
+++ src/xrSound/SoundRender_CoreA.cpp
@@ -174,8 +174,8 @@ void CSoundRender_CoreA::set_listener(const CSoundRender_Environment& env)
 			A_CHK(alEffectf(effect, AL_EAXREVERB_LFREFERENCE, env.LFReference));
 			A_CHK(alEffectf(effect, AL_EAXREVERB_DENSITY, env.Density));
 	}
-	if (snd_efx_overwrite > 5) {
-		load_reverb(effect, &reverbs[0]);}
+	if (snd_efx_overwrite > 0) {
+		load_reverb(effect, &reverbs[snd_efx_overwrite]);}
 }
 
 void CSoundRender_CoreA::get_listener(CSoundRender_Environment& env)
